ad6f0e9a81b658affd2450d847b2c835f8bbf847
Fash <favourfasi46@gmail.com>
2024-08-07 13:03:54 +0100

    Group Project

---
+++ index.js
@@ -0,0 +1,43 @@
+let html = document.getElementsByTagName("html")[0];
+let modeChanger = document.getElementById("darkMode");
+let r = document.querySelector(":root");
+modeChanger.addEventListener("click", () => {
+  if (modeChanger.classList.contains("bi-sun")) {
+    modeChanger.classList.remove("bi-sun");
+    modeChanger.classList.add("bi-moon");
+    r.style.setProperty("--white", "255, 255, 255");
+    r.style.setProperty("--black", "0, 0, 0");
+    html.removeAttribute("data-bs-theme", "light");
+    html.setAttribute("data-bs-theme", "dark");
+  } else {
+    modeChanger.classList.add("bi-sun");
+    modeChanger.classList.remove("bi-moon");
+    r.style.setProperty("--black", "255, 255, 255");
+    r.style.setProperty("--white", "0, 0, 0");
+    html.removeAttribute("data-bs-theme", "dark");
+    html.setAttribute("data-bs-theme", "light");
+  }
+});
+const data = document.getElementById('date')
+date = data.innerHTML;
+const eventDate = new Date(date).getTime();
+const countdown = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = eventDate - now;
+  document.getElementById("days").innerText = Math.floor(
+    distance / (1000 * 60 * 60 * 24)
+  );
+  document.getElementById("hours").innerText = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  document.getElementById("minutes").innerText = Math.floor(
+    (distance % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  document.getElementById("seconds").innerText = Math.floor(
+    (distance % (1000 * 60)) / 1000
+  );
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("timer").innerHTML = "Event Started";
+  }
+}, 1000);
